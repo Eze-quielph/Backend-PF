fb086c47b6cc717709a1bc57b76e248f0f3bd089
eze-quielph <ezequiel.fotografiaa@gmail.com>
2023-08-31 11:07:36 -0300

    Borrado logico terminado, Middleware añadido

--- src/Routes/Song.routes.js
+++ src/Routes/Song.routes.js
@@ -20,5 +20,6 @@ songRouter.get('/name', validateInputMiddleware, songsHandler.getByName) // GET 
 songRouter.get('/:id', validateIdMiddleware, songsHandler.getById) //GET by Id
 songRouter.put('/:id', upload.single('file'), validatePutMiddleware, songsHandler.updateSong) // name | description | genre | artist | image
 songRouter.post('/post',  upload.fields([{ name: 'image', maxCount: 1 }, { name: 'sound', maxCount: 1 }]), validatePostMiddlewae, songsHandler.postSound) // name | description | genre | artist | image | sound
+songRouter.put('/delete/:id', validateIdMiddleware, songsHandler.disableSong)
 
 module.exports = songRouter 
