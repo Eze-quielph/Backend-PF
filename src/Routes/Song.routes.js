5a20f5f2c7be30b1681055abbb36a72bfc89e97b
eze-quielph <ezequiel.fotografiaa@gmail.com>
2023-08-31 08:18:38 -0300

    Middlewares de la rutas de songs terminados

--- src/Routes/Song.routes.js
+++ src/Routes/Song.routes.js
@@ -1,20 +1,24 @@
 const songRouter = require('express').Router();
-const multer = require('multer')
 
 //Middleware
 const validateInputMiddleware = require('../Middleware/songs.Middleware/getName.middleware')
 const validateIdMiddleware = require('../Middleware/songs.Middleware/GetId.middleware')
 const validatePutMiddleware = require('../Middleware/songs.Middleware/Put.middleware')
+const validatePostMiddlewae = require('../Middleware/songs.Middleware/Post.middleware')
 
+//Handler
 const {SongsHandler} = require('../Handlers/index.handlers')
 const songsHandler = new SongsHandler();
 
+//Files
+const multer = require('multer')
 const upload = multer({ dest: 'uploads/' });
 
+//Routes
 songRouter.get('/', songsHandler.getSong) //GET All
 songRouter.get('/name', validateInputMiddleware, songsHandler.getByName) // GET by Name
 songRouter.get('/:id', validateIdMiddleware, songsHandler.getById) //GET by Id
 songRouter.put('/:id', upload.single('file'), validatePutMiddleware, songsHandler.updateSong) // name | description | genre | artist | image
-songRouter.post('/post',  upload.fields([{ name: 'image', maxCount: 1 }, { name: 'sound', maxCount: 1 }]), songsHandler.postSound) // name | description | genre | artist | image | sound
+songRouter.post('/post',  upload.fields([{ name: 'image', maxCount: 1 }, { name: 'sound', maxCount: 1 }]), validatePostMiddlewae, songsHandler.postSound) // name | description | genre | artist | image | sound
 
 module.exports = songRouter 
