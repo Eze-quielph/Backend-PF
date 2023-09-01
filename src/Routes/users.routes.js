5186adebf8f473178d8d068e9b0c8a198e58bfac
eze-quielph <ezequiel.fotografiaa@gmail.com>
2023-09-01 16:29:59 -0300

    midd

--- src/Routes/users.routes.js
+++ src/Routes/users.routes.js
@@ -1,5 +1,6 @@
 const usersRouter = require("express").Router();
 
+//Handlers
 const UserHandler = require("../Handlers/userHandler");
 const userHandler = new UserHandler();
 
@@ -11,14 +12,22 @@ const validateNameMiddleware = require("../Middleware/users.playlist/getById.mid
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
+//Routes
 usersRouter.get("/", userHandler.getUsers);
-usersRouter.get("/:id", userHandler.getUserById);
-usersRouter.delete("/:id", userHandler.deleteUser);
-usersRouter.post("/", upload.single("file"), userHandler.postUser);
-usersRouter.put("/:id", upload.single("file"), userHandler.putUser);
-
-// usersRouter.post("/", (req, res) => {
-//   res.send("estas en el post");
-// });
+usersRouter.get("/name", validateNameMiddleware, userHandler.getUsers);
+usersRouter.get("/:id", validateIdMiddleware, userHandler.getUserById);
+usersRouter.delete("/:id", validateIdMiddleware, userHandler.deleteUser);
+usersRouter.post(
+  "/",
+  upload.single("file"),
+  validateIdMiddleware,
+  userHandler.postUser
+);
+usersRouter.put(
+  "/:id",
+  upload.single("file"),
+  validateIdMiddleware,
+  userHandler.putUser
+);
 
 module.exports = usersRouter;
