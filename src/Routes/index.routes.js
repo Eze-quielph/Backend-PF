182cd94afe34be1441b6bae542be19b91186ed8e
eze-quielph <ezequiel.fotografiaa@gmail.com>
2023-09-08 16:52:27 -0300

    Variables de entorno para el front

--- src/Routes/index.routes.js
+++ src/Routes/index.routes.js
@@ -2,11 +2,33 @@ const mainRouter = require("express").Router();
 const usersRouter = require("./users.routes");
 const songRouter = require("./Song.routes");
 const playlistsRouter = require("./playlists.routes");
-const pmRoutes = require('./Premium.routes')
+const pmRoutes = require("./Premium.routes");
+
+require("dotenv").config();
+const {
+  FIRE_AUTH_DOMAIN,
+  FIRE_API_KEY,
+  FIRE_PROJECT_ID,
+  FIRE_STORAGE_BUCKET,
+  FIRE_MESSAGING_SENDER_ID,
+  FIRE_APP_ID,
+  FIRE_MEASUREMENT_ID,
+} = process.env;
 
 mainRouter.use("/users", usersRouter);
 mainRouter.use("/song", songRouter);
 mainRouter.use("/playlists", playlistsRouter);
-mainRouter.use("/premium", pmRoutes)
+mainRouter.use("/premium", pmRoutes);
+mainRouter.get("/env", (req, res) =>
+  res.status(200).json({
+    FIRE_AUTH_DOMAIN,
+    FIRE_API_KEY,
+    FIRE_PROJECT_ID,
+    FIRE_STORAGE_BUCKET,
+    FIRE_MESSAGING_SENDER_ID,
+    FIRE_APP_ID,
+    FIRE_MEASUREMENT_ID,
+  })
+);
 
 module.exports = mainRouter;
