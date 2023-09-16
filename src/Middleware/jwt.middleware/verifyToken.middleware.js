0886827800fdea6c5b4d9cb9d153bc9a4246bd7a
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2023-09-16 14:24:29 +0200

    created proteccion a post de user premium

--- src/Middleware/jwt.middleware/verifyToken.middleware.js
+++ src/Middleware/jwt.middleware/verifyToken.middleware.js
@@ -2,19 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
-
   if (!token) {
     return res.status(401).json({ message: "Token no proporcionado" });
   }
-
   const tokenBearer = token.split(" ")[1];
-
   try {
     const decoded = jwt.verify(tokenBearer, process.env.JWT_SECRET);
-
     // Agregar la información del usuario decodificada al objeto de solicitud
     req.user = decoded;
-
+    //console.log(req.user);
     // Continuar con la ejecución normal
     next();
   } catch (error) {
